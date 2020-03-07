b089c95f4d81fc0d1146f4614725fe1a3437b27f
mostafa.drz <mostafa69d@gmail.com>
2020-03-07 17:19:43 -0500

    Updates

---
+++ precache-manifest.56e10d9c8f4aef548fb33fe2474a4f49.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50e1b9cabe721dc1f191ffe72157e487",
+    "url": "/react-mobile-device-view/index.html"
+  },
+  {
+    "revision": "1ca30a501869030c72ff",
+    "url": "/react-mobile-device-view/static/css/main.ca5f27cc.chunk.css"
+  },
+  {
+    "revision": "7fb7cf1398c8fc765102",
+    "url": "/react-mobile-device-view/static/js/2.95242cf8.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/react-mobile-device-view/static/js/2.95242cf8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ca30a501869030c72ff",
+    "url": "/react-mobile-device-view/static/js/main.eca76906.chunk.js"
+  },
+  {
+    "revision": "a253e9d872dde276ce30",
+    "url": "/react-mobile-device-view/static/js/runtime-main.00341437.js"
+  }
+]);
